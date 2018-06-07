86114968eb0d34c7e6aa9797b6c32ace2e3aaae2
JayceSoul <39965757+JayceSoul@users.noreply.github.com>
2018-06-07 14:22:24 -0400

    Add files via upload

--- OJ_RPG_2018.java
+++ OJ_RPG_2018.java
@@ -5,157 +5,137 @@ import javax.swing.Timer; // Allow use of timer
 import javax.imageio.*;
 import java.io.*;
 import java.util.*;
-import java.util.Scanner;
 
-public class OJ_RPG_2018 extends JFrame 
+
+
+public class OJ_RPG_2018 extends JFrame  
 {// start
-  static Scanner sc;
-  public static void main(String[] args) 
-  {///start of main 
-    sc = new Scanner (System.in); 
-    OJ_RPG_2018 game = new  OJ_RPG_2018 ();
-    game.setVisible (true);
-    
-    
-    
-  }
+  Timer t = new Timer(80,null);// updates graphics and game
+  Map floor = new Map(0);
+  boolean walk = false;
+  Player player = new Player (2*32,21*32,0,true,"Potato",1);
+  double disx, disy;
+  int num = 0;
+//==================================<Image Method>====================================
+  public static Image loadImage (String name)  //Loads image from file
+  { Image img = null;
+    try{img = ImageIO.read (new File (name));}
+    catch (IOException e){}
+    return img;}
 //===============================<Update>===========================================
   public void update ()
+  {repaint();}
+//=====================================<>===========================================
+  class Map    
   {
-    repaint(); 
+    char[][] floor = new char[25][25];
+    String[] map = new String[] {"map1", "map2", "map3"};
+    int lvl;
+    Image pic;
+    
+    public Map (int l)throws IOException 
+    {lvl = l;
+      this.loadMap();}
+    
+    public void loadMap() throws IOException 
+    {
+      File level = new File("res/text/maps/" + map[lvl] + ".txt");
+      pic = loadImage("res/images/maps/" + map[lvl] + ".png");
+      Scanner sc = new Scanner(level);
+      String  line="";
+      for (int y = 0; y < 25; y++) 
+      { line = sc.nextLine();
+        for (int x = 0; x < 25; x++)
+        {floor[x][y] = line.charAt(x);}}}
+    
+    public Image getMap()
+    {return pic;}
+    
+    public char getFloorID (int x, int y)
+    {return floor[x][y];}
+    
   }
-  //=================================<Player class>============================== 
-  
-  class Player 
-  {
-    int identity; //what is it
-    int x, y; //position
-    boolean status, setGender; //true for female, false for male;
-    String setName; //player name stats
-    private boolean [] inventory = new boolean [3]; //stores 
-    int direction; //int values for directions (0 is up, 1 is down, 2 is right, 3 is left)
     
-    public Player (int x, int y, int id, boolean gender, String name) { //constructor
-      identity = id;
-      this.x = x;
-      this.y = y;
-      status = true;
-      setGender = gender;
-      setName = name;
-    }
-    
-    public int getID() { //retrieve the identity of the player to see whether the object is a player or enemy (player=1, enemy=0)
-      return identity; 
-    }
-    
-    public int getX() { //retrieve the players y position 
-      return x; 
-    }
-    public int getY() { //retrieve the players x position
-      return y; 
-    }
-    public void changeX(int num) { //change the enemy's x position
-      x+=num; 
-      if (num >0) //if positive
-        direction = 2; //right
-      else
-        direction = 3; //left
-    }
-    public void changeY(int num) { //change the enemy's y position
-      y+=num; 
-      if (num >0) //if positive
-        direction = 0; //up
-      else
-        direction = 1; //down
-    }
-    
-    public boolean getStatus() { //to see if the player is alive
-      return status;
-    }
-    
-    public boolean getGender() { //if it is true then female, false=male
-      return setGender;
-    }
-    public void getItem(int id){ //retrieve key from inventory array
-      inventory[id] = true; 
-    }
-    public int getDirection(int num){ //draw class will have 4 numbers each assigned an arrow key
-      return num;   
-    }
-  }
-  
-  class Item {
-    boolean object;
-    int ID, xpos,ypos;
-    //changing x and y coordinates for the memory objets which will follow the monster
-    
-    public Item (int identity, int x, int y) { //constructor
-      ID = identity; //the id is like 1 for sword, 0 for key and 2 for memory objects
-      object = true; //change boolean for object being on the screen to true
-      xpos= x; 
-      ypos=y;
+    class Player {
+      int identity;
+      int xpos, ypos;
+      int velx, vely;
+      boolean status, setGender; //true for female, false for male;
+      String setName;
+      private boolean [] inventory = new boolean [3]; //stores 
+      int direction;
       
+      public Player (int x, int y, int id, boolean gender, String name, int dir) { //constructor
+        identity = id;
+        xpos=x;
+        ypos=y;
+        status = true;
+        setGender = gender;
+        setName = name;
+        direction = dir;
+      }
+      public int getID() { //retrieve the identity of the player to see whether the object is a player or enemy (player=1, enemy=0)
+        return identity; 
+      }
+      public int getX() { //retrieve the players y position 
+        return xpos; 
+      }
+      public int getY() { //retrieve the players x position
+        return ypos; 
+      }
+       public int getVelX() { //retrieve the players y position 
+        return velx; 
+      }
+        public int getVelY() { //retrieve the players y position 
+        return vely; 
+      } 
+      public void changeX(int num) { //change the enemy's x position
+        int x = xpos+velx ,y = ypos +vely;
+
+      if (num != 0)
+      {if (num > 0)//if positive
+        { if (floor.getFloorID((x+24)/32 ,(y+24)/32) == 'f')// && floor.getFloorID(x/32 ,(y-24)/32) != 'w')
+      {xpos += num;}
+        direction = 2;} //right
+      else 
+      { if (floor.getFloorID((x)/32 ,(y+24)/32) == 'f')// && floor.getFloorID(x/32 ,(y-24)/32) != 'w')
+      {xpos += num;}
+        direction = 3;}} //left
     }
-    public int getx (){ //x position of the object
-      return xpos;
-    }
-    
-    public int gety () {//y position of the object
-      return ypos;    
-    }
-    
-    public void pickUp() { //if the object is picked by the player such as if they walk over it then the boolean for the object being on the screen is changed to false
-      object= false;
-    }
-  }
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////  
-  class Enemy {
-    private int identity; //the identity 
-    private int x, y; //position
-    private boolean status; //true for alive, false for dead;
-    private int direction; //int values for directions (0 is up, 1 is down, 2 is right, 3 is left)
-    
-    public Enemy (int x, int y, int id) { //constructor
-      identity = id;
-      this.x = x;
-      this.y = y;
-      status = true;
-    }
-    
-    public int getID() { //retrieve the identity of the enemy to see whether the object is a player or enemy (player=1, enemy=0)
-      return identity; 
-    }
-    public int getX() { //retrieve the enemy's y position 
-      return x; 
-    }
-    public int getY() { //retrieve the enemy's x position
-      return y; 
-    }
-    public void changeX(int num) { //change the enemy's x position
-      x+=num; 
-      if (num >0) //if positive
-        direction = 2; //right
+     public void changeY(int num) { //change the enemy's y position
+       int x = xpos+velx ,y = ypos +vely;
+       
+      if(num !=0)
+      {if (num >0) //if positive
+        { if (floor.getFloorID((x+24)/32,(y+24)/32) != 'w' && floor.getFloorID(x/32 ,(y+32)/32) != 'w')
+        {ypos += num;}
+        direction = 1;} //down
       else
-        direction = 3; //left
+      { if (floor.getFloorID((x+24)/32,y/32)!= 'w' && floor.getFloorID(x/32 , (y+24)/32) != 'w')
+      {ypos += num;}
+        direction = 0;} }//up
     }
-    public void changeY(int num) { //change the enemy's y position
-      y+=num; 
-      if (num >0) //if positive
-        direction = 0; //up
-      else
-        direction = 1; //down
+     
+    public void setVelX(int vel)
+    {velx=vel;}
+    public void setVelY(int vel) 
+    {vely=vel;}
+      public boolean getStatus() { //to see if the player is alive
+        return status;
+      }
+      
+      public boolean getGender() { //if it is true then female, false=male
+        return setGender;
+      }
+      public void getItem(int num){ //change item entered (based on the number ID) to true so the player has picked up the object
+        inventory [num] = true;
+      }  
+      public int getDirection(){ //draw class will have 4 numbers each assigned an arrow key
+        return direction;  
+      }
     }
-    public boolean getStatus() { //to see if the enemy is alive
-      return status;
-    }
-    
-    public int getDirection(){ //draw class will have 4 numbers each assigned an arrow key
-      return direction;  
-    }
-    
-    
 //=================================<Panel constructor>============================== 
-    public void OJ_RPG_2018 () 
+    public OJ_RPG_2018 () throws IOException
     {// start of panel setup
       // Creating JPanel for the background and JPanel for interations (options)
       setLayout (new BorderLayout ());// Use BorderLayout for main panel
@@ -166,9 +146,10 @@ public class OJ_RPG_2018 extends JFrame
       // Colouring panels  
       background.setBackground(Color.BLACK);
       //timer setup   
-      
+      t.addActionListener(new TimerListener());
+      background.addKeyListener(new playerListener());
       // Creating Drawing Area and Panel Setup
-      DrawArea playfield = new DrawArea (900, 600);
+      DrawArea playfield = new DrawArea (1000, 1000);
       background.add(playfield); // Output area
       add("Center",background);
       setContentPane(background);
@@ -176,8 +157,8 @@ public class OJ_RPG_2018 extends JFrame
       pack();
       // Game window Setup  
       setTitle("Hangman Plus");
-      background.setPreferredSize (new Dimension (900,600));
-      setSize(1080,740); //Sets the JFrame size
+      background.setPreferredSize (new Dimension (1000,800));
+      setSize(1080,825); //Sets the JFrame size
       setVisible(true); //Reveals JFrame
       setResizable(false); 
       setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
@@ -195,8 +176,36 @@ public class OJ_RPG_2018 extends JFrame
       
       public void paintComponent (Graphics g)  // g can be passed to a class method
       {//start of paintComponent
+        Image[][][] tony = loadPlayer();
+        int x,y,z;
+        if (player.getGender() == true)
+        {z=0;}
+        else {z=1;}
         
+        y = player.getDirection();
+        //System.out.println("<"+y+">");
+        g.drawImage(floor.getMap(),0,0,null);
+//           for (int a = 0; a < 25; a++) 
+//     {for (int b = 0; b < 25; b++)
+//     {if (floor.getFloorID(b,a) =='f')
+//       {g.setColor(Color.white);
+//       g.fillRect(0+(32*b),0+(32*a),32,32);}}}
+               
+        if (!walk)
+        {g.drawImage(tony[z][y][0], player.getX(), player.getY()-16,null);}
+        else{g.drawImage(tony[z][y][num],player.getX(), player.getY()-16,null);}
+
       }
+      
+      public Image[][][] loadPlayer()
+      {
+        Image[][][] player = new Image[2][4][3];//z/y/x
+        for (int z = 0; z < 1; z++)
+        {for (int y = 0; y < 4; y++)
+        {for (int x = 0; x < 3; x++)
+          {player[z][y][x] = loadImage("res/images/player/"+z+"/"+y+x+".png");
+        }}}
+      return player;}
       
     }//end of drawarea
     
@@ -210,25 +219,100 @@ public class OJ_RPG_2018 extends JFrame
 //==================================================================================
     class TimerListener implements ActionListener //reacts to timer
     {//start
+      int steps = 0;
       TimerListener()
       {//start
-        //t.start();//starts timer
+        t.start();//starts timer
       }//end
       public void actionPerformed (ActionEvent e)
       {//start of void
-        update();//updates graphics
-//      if (false)
-//      {
-//        stop = true;
-//      }
-//      
-//      if (true)
-//      {
-//        stop = false;
-//      }
-        
+       
+       update();//updates graphics
+       
+      //if (walk)
+      //{
+        steps();
+        player.changeY(player.getVelY());
+        player.changeX(player.getVelX());//}
+      //else{}
       }//end of void
-    }//end of class 
-    
-  }///end of class
-}
+      
+      
+      public void steps()
+     {
+       
+       //try{
+         // Thread.sleep(5);}
+        //catch(InterruptedException e){}
+       
+       steps++;
+          if (steps == 1)
+          { num = 1;}
+        else if (steps == 2)
+        {num = 0;}
+        else if (steps == 3)
+        { num = 2;}
+        else if (steps == 4)
+        {num = 0;}
+        else {num = 0;
+        steps = 0;}
+//        try{
+//          Thread.sleep(5);}
+//        catch(InterruptedException e){}
+        
+     }
+      
+      
+    }//end of class
+//==============================================================================
+    class playerListener implements KeyListener
+    {
+      
+      public playerListener()       {}
+      
+      @Override
+      public void keyTyped(KeyEvent e) { // respond to keys typed
+        
+      }
+      
+      @Override
+      public void keyPressed(KeyEvent e) 
+  {
+       
+        //System.out.println("hi");
+        walk = true;
+        int x, y; 
+        x = player.getX();
+        y = player.getY();
+        //System.out.println(code);
+        if(e.getKeyCode() == KeyEvent.VK_UP )//&& floor.getFloorID(x,y-1) != 'w')
+        {player.setVelY(-8);}
+        else if(e.getKeyCode() == KeyEvent.VK_DOWN)// && floor.getFloorID(x,y+1) != 'w')
+        {player.setVelY(8);}
+        else if(e.getKeyCode() == KeyEvent.VK_LEFT)// && floor.getFloorID(x-1,y) != 'w')
+        {player.setVelX(-8);}
+         else if(e.getKeyCode() == KeyEvent.VK_RIGHT)// && floor.getFloorID(x+1,y) != 'w')
+         {player.setVelX(8);}
+      }
+      @Override
+      public void keyReleased(KeyEvent e) { // All interface methods must be defined
+        
+        if(e.getKeyCode() == KeyEvent.VK_UP)
+        {player.setVelY(0);}
+        else if(e.getKeyCode() == KeyEvent.VK_DOWN)
+        {player.setVelY(0);}
+        else if(e.getKeyCode() == KeyEvent.VK_LEFT)
+        {player.setVelX(0);}
+        else if(e.getKeyCode() == KeyEvent.VK_RIGHT)
+        {player.setVelX(0);}  
+        walk = false;}
+      
+     
+  }
+//==================================================================================    
+  public static void main(String[] args) throws IOException
+  {///start of main
+    OJ_RPG_2018 game = new  OJ_RPG_2018 ();
+    game.setVisible (true);
+  }//end of main
+}///end of class
